215542f1016c8e880db46e5aeecf07dbefdbf7d6
mustafa jawadwala <mustafajw07@gmail.com>
2022-06-20 19:47:32 +0530

    navbar

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import About from "./routes/About";
 import Home from "./routes/Home";
 import Products from "./routes/Products";
 import Contact from "./routes/Contact";
+import Error from "./routes/Error";
 
 
 function App() {
@@ -18,6 +19,7 @@ function App() {
         <Route path="/about" element={<About />} />
         <Route path="/products" element={<Products />} />
         <Route path="/contact" element={<Contact />} />
+        <Route path="*" element={<Error />} />
       </Routes>
     </Router>
     </>
---
+++ src/routes/Error.js
@@ -0,0 +1,7 @@
+import nf from "../asserts/images/nf.svg"
+
+function Error() {
+  return <img src={nf}/>;
+}
+
+export default Error;
