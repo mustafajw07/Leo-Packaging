309f885c451719edcf73aadcb900ec4fa962bda8
mustafa jawadwala <mustafajw07@gmail.com>
2022-06-22 12:41:15 +0530

    error

--- src/routes/Error.js
+++ src/routes/Error.js
@@ -1,13 +1,10 @@
-// import { Link } from "react-router-dom";
 import Wrapper from "../asserts/styles/Error";
-import nf from "../asserts/images/nf.svg"
-
 
 function Error() {
   return <Wrapper>
   <div id="notfound">
-		<div class="notfound">
-			<div class="notfound-404">
+		<div className="notfound">
+			<div className="notfound-404">
 				<h3>Oops! Page not found</h3>
 				<h1><span>4</span><span>0</span><span>4</span></h1>
 			</div>
