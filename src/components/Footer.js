241a1d01436b3fe4b1cb3d266a2eb0027df93b55
mustafa jawadwala <mustafajw07@gmail.com>
2022-06-22 15:18:11 +0530

    footer

---
+++ src/components/Footer.js
@@ -0,0 +1,29 @@
+import Wrapper from "../asserts/styles/Footer"
+import {Link} from "react-router-dom"
+
+function Footer() {
+  return <Wrapper>
+  <div className="footer">
+  {/* <div className="row">
+  <Link to="#"><i className="fa fa-facebook"></i></Link>
+  <Link to="#"><i className="fa fa-instagram"></i></Link>
+  <Link to="#"><i className="fa fa-youtube"></i></Link>
+  <Link to="#"><i className="fa fa-twitter"></i></Link>
+  </div> */}
+  
+  <div className="row">
+  <ul>
+  <li><Link to="/contact">Contact us</Link></li>
+  <li><Link to="/about">About Us</Link></li>
+  <li><Link to="/products">Our Products</Link></li>
+  </ul>
+  </div>
+  
+  <div className="row">
+  AL-ZAMAN Copyright © 2022 Al-zaman - All rights reserved || Designed By: Mustafa
+  </div>
+  </div>
+  </Wrapper>;
+}
+
+export default Footer;
