1e9d18368e272335990215c88aa4d2f51755d0d9
mustafa jawadwala <mustafajw07@gmail.com>
2022-07-05 13:42:59 +0530

    home

--- src/components/home/Video.js
+++ src/components/home/Video.js
@@ -2,20 +2,22 @@ import Wrapper from "../../asserts/styles/home/video"
 
 function Video() {
   return <Wrapper>
-  <header className="head">
-<div className="overlay"></div>
-<video playsInline="playsinline" autoPlay="autoplay" muted="muted" loop="loop">
-  <source src="https://storage.googleapis.com/coverr-main/mp4/Mt_Baker.mp4" type="video/mp4" />
-</video>
-<div className="container">
-  <div className="video">
-    <div>
-      <h1>Video Header</h1>
-      <p className="lead mb-0">Using HTML5 Video and Bootstrap</p>
-    </div>
-  </div>
-</div>
-</header>
+    <header className="head">
+      <div className="overlay"></div>
+      <video playsInline="playsinline" autoPlay="autoplay" muted="muted" loop="loop">
+        <source src="https://storage.googleapis.com/coverr-main/mp4/Mt_Baker.mp4" type="video/mp4" />
+      </video>
+      <div className="container">
+        <div className="video">
+          <div>
+            <h1>Al-Zaman</h1>
+            <p>Al Zaman Packs is one of the leading
+              Paper Core and Corrugated Sheet
+              manufacturers in UAE.</p>
+          </div>
+        </div>
+      </div>
+    </header>
   </Wrapper>;
 }
 
