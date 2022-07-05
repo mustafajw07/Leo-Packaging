a127dee82c4a8d503102a9483064c93c3e0ef4c7
mustafa jawadwala <mustafajw07@gmail.com>
2022-07-05 13:39:54 +0530

    home

--- src/components/home/Video.js
+++ src/components/home/Video.js
@@ -3,15 +3,15 @@ import Wrapper from "../../asserts/styles/home/video"
 function Video() {
   return <Wrapper>
   <header className="head">
-<div class="overlay"></div>
+<div className="overlay"></div>
 <video playsinline="playsinline" autoplay="autoplay" muted="muted" loop="loop">
   <source src="https://storage.googleapis.com/coverr-main/mp4/Mt_Baker.mp4" type="video/mp4" />
 </video>
-<div class="container">
-  <div class="video">
+<div className="container">
+  <div className="video">
     <div>
       <h1>Video Header</h1>
-      <p class="lead mb-0">Using HTML5 Video and Bootstrap</p>
+      <p className="lead mb-0">Using HTML5 Video and Bootstrap</p>
     </div>
   </div>
 </div>
