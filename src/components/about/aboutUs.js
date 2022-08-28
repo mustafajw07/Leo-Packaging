38c6b373b0f54408a2b127efe95e972f4efa7e42
mustafa jawadwala <mustafajw07@gmail.com>
2022-08-28 12:31:08 +0530

    changes

--- src/components/about/aboutUs.js
+++ src/components/about/aboutUs.js
@@ -5,7 +5,7 @@ function aboutUs() {
   return (
     <Wrapper>
       <div className="container">
-        <p className="paragraph">
+        <div className="paragraph">
           <h1>ABOUT US</h1>
           <br />
           Lulu Packaging is one among the most important producers of top
@@ -23,7 +23,7 @@ function aboutUs() {
             know their specific needs, and <br /> partner with them to make
             packs that stand out on the shelves and shape consumer experiences.
           </p>
-        </p>
+        </div>
         <img
           className="size"
           src="https://static.wixstatic.com/media/nsplsh_5633596e46795a53473551~mv2.jpg/v1/fill/w_736,h_1148,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/nsplsh_5633596e46795a53473551~mv2.jpg"
