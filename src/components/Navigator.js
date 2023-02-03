32a9558e1b96f10d4f420eb15e9148a9205ca6d0
mustafa jawadwala <mustafajw07@gmail.com>
2023-02-03 11:20:11 +0530

    minor changes

--- src/components/Navigator.js
+++ src/components/Navigator.js
@@ -21,7 +21,7 @@ function Navigator() {
               <Link to="/products">Products</Link>
             </li>
             <li>
-              <Link to="/about">About</Link>
+              <Link to="/about">Our Legacy</Link>
             </li>
             <li>
               <Link to="/contact">Contact</Link>
